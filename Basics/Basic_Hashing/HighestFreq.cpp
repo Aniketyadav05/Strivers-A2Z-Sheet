9682088421417ef84d3ae0d66f136e15b5e6184e
Aniket Yadav <yadavaniket7611@gmail.com>
2025-03-22 15:51:50 +0530

    hashing

---
+++ Basics/Basic_Hashing/HighestFreq.cpp
@@ -0,0 +1,43 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <unordered_map> 
+using namespace std;
+
+void Frequency(int arr[], int n)
+{
+    unordered_map<int, int> mpp;
+
+    for (int i = 0; i < n; i++)
+        mpp[arr[i]]++;
+
+    int maxFreq = 0, minFreq = n;
+    int maxEle = 0, minEle = 0;
+    // Traverse through map to find the elements.
+    for (auto it : mpp) {
+        int count = it.second;
+        int element = it.first;
+
+        if (count > maxFreq) {
+            maxEle = element;
+            maxFreq = count;
+        }
+        if (count < minFreq) {
+            minEle = element;
+            minFreq = count;
+        }
+    }
+
+    cout << "The highest frequency element is: " << maxEle << "\n";
+    cout << "The lowest frequency element is: " << minEle << "\n";
+}
+
+int main()
+{
+    int arr[] = {10, 5, 10, 15, 10, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    Frequency(arr, n);
+    return 0;
+}
