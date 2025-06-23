18f18c0d60aadd4075f0f9a4b64245fe8d77a258
Aniketyadav05 <yadavaniket7611@gmail.com>
2025-06-24 00:17:43 +0530

    Reverse a LL

---
+++ LinkedList/reverseList.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+using namespace std;
+ListNode* reverseList(ListNode* head){
+    ListNode *newHead = NULL;
+    while(head!= NULL){
+        ListNode *next = head->next;
+        head->next = newHead;
+        newHead = head;
+        head = next;
+    }
+    return newHead;
+}
